27ce17fe684e05bda88f31457f9e14347422e88b
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2026-09-17 23:34:59 -0300

    Inicio do projeto

---
+++ src/main/kotlin/com/viniciusdevassis/Startup.kt
@@ -0,0 +1,11 @@
+package com.viniciusdevassis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestWithSpringBootAndKotlinErudioCourseApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestWithSpringBootAndKotlinErudioCourseApplication>(*args)
+}
---
+++ src/test/kotlin/com/viniciusdevassis/StartupTests.kt
@@ -0,0 +1,13 @@
+package com.viniciusdevassis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestWithSpringBootAndKotlinErudioCourseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
